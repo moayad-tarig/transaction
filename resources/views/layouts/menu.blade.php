1434f1e3b060e1c42366986a81ecac41ba6e275f
moayad-tarig <software.moayad@gmail.com>
2022-02-07 09:00:43 -0800

    setup a dashbord theme

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,51 @@
+<li class="c-sidebar-nav-item">
+    <a class="c-sidebar-nav-link c-active" href="{{ route('home') }}">
+        <i class="c-sidebar-nav-icon cil-home"></i> @lang('menu.home')
+    </a>
+</li>
+
+
+<li class="c-sidebar-nav-item mt-1">
+    <a class="c-sidebar-nav-link c-active" href="#">
+        <i class="c-sidebar-nav-icon cil-user"></i> طلب تحويل
+    </a>
+</li>
+<li class="c-sidebar-nav-item mt-1">
+    <a class="c-sidebar-nav-link c-active" href="#">
+        <i class="c-sidebar-nav-icon cil-user"></i> سجل المعاملات اليومية
+    </a>
+</li>
+<li class="c-sidebar-nav-item mt-1">
+    <a class="c-sidebar-nav-link c-active" href="#">
+        <i class="c-sidebar-nav-icon cil-user"></i> سجل الأمانات 
+    </a>
+</li>
+<li class="c-sidebar-nav-item mt-1">
+    <a class="c-sidebar-nav-link c-active" href="#">
+        <i class="c-sidebar-nav-icon cil-user"></i>  <div class="btn-group ">
+            <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              إدارة النظام
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+              <button class="dropdown-item" type="button">الفروع</button>
+              <button class="dropdown-item" type="button">إدارة المستخدمين</button>
+            </div>
+          </div> 
+    </a>
+</li>
+<li class="c-sidebar-nav-item mt-1">
+    <a class="c-sidebar-nav-link c-active" href="#">
+        <i class="c-sidebar-nav-icon cil-user"></i>  <div class="btn-group ">
+            <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              تقارير
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+              <button class="dropdown-item" type="button">تقارير الصادر</button>
+              <button class="dropdown-item" type="button">تقرير كامل    </button>
+            </div>
+          </div> 
+    </a>
+</li>
+
+
+
